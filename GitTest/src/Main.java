74352672a2cbe3cae4119f4e87bebe423242bf14
aa <aa@DESKTOP-OCIGRN5>
2017-10-18 14:06:23 +0900

    main메소드에 출력문 추가

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		System.out.println("Hi world");
+		System.out.println("ÀÚ¹Ù");
 	}
 
 }
